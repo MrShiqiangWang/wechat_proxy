c072e23b91c5f56ed29083baf5aa9414a10f28fa
福态 <shiqiangwang.wsq@alibaba-inc.com>
2022-04-13 13:42:27 +0800

    填充用户信息

--- tools/src/main/java/com/wangsq/tools/WrapThreadUtils.java
+++ tools/src/main/java/com/wangsq/tools/WrapThreadUtils.java
@@ -3,40 +3,6 @@ package com.wangsq.tools;
 /**
  * @author 福泰
  * @version WrapThreadUtils.java, v 0.1 2022年04月13日 11:44 AM 福泰
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- *
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
- */
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
  */
 
 import com.google.common.collect.Lists;
@@ -45,14 +11,11 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
-/**
- * @author futai
- * @version : ThreadUtilsWrap, v 0.1 2021-11-13 上午12:41 futai Exp $
- */
 public class WrapThreadUtils {
 
     /**
      * 执行线程
+     *
      * @param completionStages
      * @param <T>
      * @return
